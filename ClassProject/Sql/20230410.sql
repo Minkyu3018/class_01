ff2082201316a24cb7a5066fc58d6e0af2263bb0
Minkyu3018 <koreamanggu@gmail.com>
2023-04-10 18:08:35 +0900

    Create 20230410.sql

---
+++ ClassProject/Sql/20230410.sql
@@ -0,0 +1,11 @@
+-- 2023.04.10
+-- 주석
+
+-- 사용자 생성
+
+ALTER SESSION SET "_ORACLE_SCRIPT"=TRUE;
+
+-- 1. 다른사용자 -> 팝업메뉴 -> 사용자 생성
+-- 2. 계정이름 C##himeia -> C##을 생략하기 위해
+-- ALTER SESSION SET "_ORACLE_SCRIPT"=TRUE;   -- 실행 
+-- 3. 권한부여 : connect, resource

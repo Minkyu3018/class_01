83509cea1b9ca2e097108b0bc34b208c61d678df
Minkyu3018 <koreamanggu@gmail.com>
2023-04-19 23:37:36 +0900

    2023.04.19(Home)

---
+++ JavaProject/JDBCProject/src/JDBCProjectHome/ConnectionTest01.java
@@ -0,0 +1,13 @@
+package JDBCProjectHome;
+
+public class ConnectionTest01 {
+
+	public static void main(String[] args) {
+
+	//	Class.forName("oracle.jdbc.driver.OracleDriver");
+		
+		
+		
+	}
+
+}

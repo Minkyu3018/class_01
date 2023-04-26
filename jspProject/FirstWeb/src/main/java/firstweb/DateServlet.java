ca819564325f6ab78394082556e1c62d0fd578de
Minkyu3018 <koreamanggu@gmail.com>
2023-04-26 18:19:51 +0900

    2023.04.26(Study)

--- jspProject/FirstWeb/src/main/java/firstweb/DateServlet.java
+++ jspProject/FirstWeb/src/main/java/firstweb/DateServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet({ "/now", "/test/now" })
+/*@WebServlet({ "/now", "/test/now" })*/
 public class DateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        

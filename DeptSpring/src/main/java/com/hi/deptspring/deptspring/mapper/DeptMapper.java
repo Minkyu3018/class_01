ebe50e491ed8becd9246ffec5d64faea4b59bdf0
Minkyu3018 <koreamanggu@gmail.com>
2023-05-17 18:17:54 +0900

    2023.5.17(Spring/REST)

--- DeptSpring/src/main/java/com/hi/deptspring/deptspring/mapper/DeptMapper.java
+++ DeptSpring/src/main/java/com/hi/deptspring/deptspring/mapper/DeptMapper.java
@@ -2,6 +2,7 @@ package com.hi.deptspring.deptspring.mapper;
 
 import com.hi.deptspring.deptspring.domain.DeptDTO;
 import com.hi.deptspring.deptspring.domain.DeptRegistRequest;
+import com.hi.deptspring.deptspring.domain.DeptSearchOption;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -12,11 +13,19 @@ public interface DeptMapper {
     // 전체 부서 리스트
     List<DeptDTO> selectAll();
 
+    List<DeptDTO> selectAll2();
+
+    List<DeptDTO> selectByOption(DeptSearchOption option);
+
     // deptno 컬럼으로 검색 => DeptDTO
     DeptDTO selectByDeptno(int deptno);
 
+    List<DeptDTO> selectByDeptnos(List<Integer>deptnos);
+
     // DeptRegistRequest 를 받아서 insert 해주는 메소드
     int insertDept(DeptRegistRequest request);
+
+    int insertDept2(DeptDTO deptDTO);
 
     // DeptDTO 전달 받고 update 메소드
     int updateDept(DeptDTO deptDTO);
--- DeptSpring/src/test/java/com/hi/deptspring/ConnectionTest.java
+++ DeptSpring/src/test/java/com/hi/deptspring/ConnectionTest.java
@@ -1,6 +1,7 @@
 package com.hi.deptspring;
 
 import com.hi.deptspring.deptspring.domain.DeptDTO;
+import com.hi.deptspring.deptspring.domain.DeptSearchOption;
 import com.hi.deptspring.deptspring.mapper.DeptMapper;
 import com.hi.deptspring.deptspring.mapper.TimeMapper;
 import com.hi.deptspring.deptspring.mapper.TimeMapper2;
@@ -15,6 +16,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Log4j2
@@ -36,8 +38,70 @@ public class ConnectionTest {
     private DeptMapper deptMapper;
 
     @Test
+    public void selectByDeptnosTest(){
+
+        List<Integer> deptnos = new ArrayList<>();
+        deptnos.add(10);
+        deptnos.add(30);
+        deptnos.add(50);
+        deptnos.add(53);
+
+        List<DeptDTO> list = deptMapper.selectByDeptnos(deptnos);
+        log.info("lsit : " + list);
+
+    }
+
+    @Test
+    public void searchDeptTest(){
+
+        DeptSearchOption option1 = DeptSearchOption
+                .builder()
+                .searchType("dname")
+                .keyword("A")
+                .build();
+
+        DeptSearchOption option2 = DeptSearchOption
+                .builder()
+                .searchType("loc")
+                .keyword("B")
+                .build();
+
+        DeptSearchOption option3 = DeptSearchOption
+                .builder()
+                .searchType("")
+                .keyword("")
+                .build();
+        
+        // 부서 이름 검색
+        List<DeptDTO> list1 = deptMapper.selectByOption(option1);
+        log.info(list1);
+        
+        // 부서 위치 검색
+        List<DeptDTO> list2 = deptMapper.selectByOption(option2);
+        log.info(list2);
+
+        // 검색어가 없는 검색
+        List<DeptDTO> list3 = deptMapper.selectByOption(option3);
+        log.info(list3);
+
+    }
+
+    @Test
+    public void deptInsertTest(){
+        DeptDTO dept = DeptDTO.builder().dname("test").loc("서울").build();
+        log.info(">>>객체 생성 : " + dept);
+
+        deptMapper.insertDept2(dept);
+        log.info(">>>insert 후 DeptDTO : " + dept);
+        // dept.getDeptno() => 다른 테이블의 FK 값으로 사용
+        // insert
+        // insert
+    }
+
+    @Test
     public void getDeptListTest(){
-        List<DeptDTO> list = deptMapper.selectAll();
+//        List<DeptDTO> list = deptMapper.selectAll();
+        List<DeptDTO> list = deptMapper.selectAll2();
         log.info(">>>>>>>" + list);
 
         log.info(deptMapper.selectByDeptno(10));
